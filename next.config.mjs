f0c8aade7181e49c94432afddeeeebe11999caec
Daniil Robnikov <daniilrobnikov@gmail.com>
2022-12-04 02:01:06 +0200

    Husky with build

---
+++ next.config.mjs
@@ -0,0 +1,25 @@
+// @ts-check
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
+ * This is especially useful for Docker builds.
+ */
+!process.env.SKIP_ENV_VALIDATION && (await import("./src/env/server.mjs"));
+
+// process.env.ANALYZE && 
+import withBundleAnalyzer from '@next/bundle-analyzer';
+
+const bundleAnalyzer = withBundleAnalyzer({
+	enabled: process.env.ANALYZE === 'true',
+})
+
+/** @type {import("next").NextConfig} */
+const config = {
+  reactStrictMode: true,
+  swcMinify: true,
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
+};
+
+export default bundleAnalyzer(config);
